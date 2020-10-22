6894679e739d6fd5670e5626ca6b33340d85ff2b
josolo9909 <jose.solanol@udea.edu.co>
2020-10-22 09:18:41 -0500

    Arreglado

--- main.cpp
+++ main.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 
 //Variables globales y macros
-#define TAM (int)10
+#define TAM (int)50
 
 using namespace std;
 
 int main()
 {
-    char a[TAM] = "AB11CD44";
+    char a[TAM] = "AB11CD4418548X48";
 
     for (int var = 0;var<TAM ;++var ) {
+        if (a[var]!= '\0'){
         cout << a[var] << endl;
+}
 
     }
     cout << "Imprimir solo los numeros de la cadena original"<< endl;
