963060194aaee3985db3d893cff4731e80aa0de4
josolo9909 <jose.solanol@udea.edu.co>
2020-10-22 09:09:58 -0500

    Creacion de rama

--- main.cpp
+++ main.cpp
@@ -8,8 +8,16 @@ using namespace std;
 int main()
 {
     char a[TAM] = "AB11CD44";
+
     for (int var = 0;var<TAM ;++var ) {
         cout << a[var] << endl;
+
+    }
+    cout << "Imprimir solo los numeros de la cadena original"<< endl;
+    for (int var = 0;var<TAM ;++var ) {
+        if ( a[var]>='0' and a[var]<= '9'){
+            cout << a[var] << endl;
+        }
 
     }
     return 0;
